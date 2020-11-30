5f69327ad3df13412a944e3a88b354c48048189c
Erik Sundell <erik@sundellopensource.se>
2020-11-30 18:03:00 +0100

    Exclude duplicated conda env from showing up

--- chart/files/usr/local/etc/jupyter/jupyter_notebook_config.py
+++ chart/files/usr/local/etc/jupyter/jupyter_notebook_config.py
@@ -9,3 +9,16 @@
 #     'command': ['python3', '-m', 'http.server', '--directory', '/nh/curriculum/we-nilearn-dupre/book', '{port}'],
 #   }
 # }
+
+# To allow intense communication between browser and the server, as required
+# for example by plotly and itk-jupyter-widgets with 3D things.
+c.NotebookApp.iopub_data_rate_limit = 1e12
+
+# Configuration of nb_conda_kernels that converts conda environments to conda
+# kernels, but only those that are not already listed.
+#
+# ref: https://github.com/Anaconda-Platform/nb_conda_kernels
+c.CondaKernelSpecManager.name_format = '{environment}'
+# Let's avoid using a base kernel alongside the notebook kernel, they should be
+# the same. The expression matches everything except that specific env.
+c.CondaKernelSpecManager.env_filter = '^((?!/srv/conda/envs/notebook).)*$'
